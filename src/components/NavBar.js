158cddfeee084dc0380d6f92840f7e4c6972fae7
adarsh25t <adarsh25t@gmail.com>
2022-01-10 23:48:16 +0530

    add new navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,76 +1,33 @@
-import { useState } from "react";
+import React, { useState} from "react";
 import "./NavBar.css";
 
-const NavBar = () => {
 
-  const [login,setLogin] = useState(false);
-  const [active,setActive] = useState(false);
+const Navbar = () => {
+  const [active, setActive] = useState(false);
   
-  const MobileNav = ()=>{
-      setActive(!active)
-  }
 
+  const handleClick = () => {
+    setActive(!active);
+  };
+ 
   return (
-    <nav class="navbar navbar-expand-lg ">
-      <div class="container">
-        <a class="navbar-brand" href="#">
-          Shop now
-        </a>
-        <button onClick={MobileNav}
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i class="fas fa-bars"></i>
-        </button>
-        <div className={active ?"active" : "collapse navbar-collapse"} id="navbarSupportedContent">
-        <div class="input-group mb-3 search">
-            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-            <span class="input-group-text " id="basic-addon2"><i class="fas fa-search"></i></span>
-        </div>
-          <ul class="navbar-nav  mb-2 mb-lg-0">
-             {!login ? <li class="nav-item login">
-              <a class="nav-link login-btn" aria-current="page" href="#">
-                Login
-              </a>
-            </li> : <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr class="dropdown-divider" />
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>}
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-              <i class="fas fa-shopping-cart"></i> Cart
-              </a>
-            </li>
-          </ul>
-        </div>
+    <nav className="navbar">
+      <div className="container">
+      <h3 className="nav-logo">shop <span>now</span></h3>
+      
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={active ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+
+      <input type="text" className="search" placeholder="search"/>
+      <div className={active ? "nav-menu active" : "nav-menu"}>
+         <input type="text" className="nav-search" placeholder="search"/>
+          <button className="login">Login</button>
+          <button className="cart"><span>0</span><i class="fas fa-shopping-cart"></i> Cart</button>
+      </div>
       </div>
     </nav>
   );
 };
-export default NavBar;
+
+export default Navbar;
